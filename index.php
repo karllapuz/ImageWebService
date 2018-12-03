1abb75c7c1cb3491f562b7b3a17418a605e1961d
Karl Lapuz <karllapuz25@gmail.com>
2018-12-03 15:10:34 -0800

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mika</title>
+    <script   src="https://code.jquery.com/jquery-3.3.1.min.js"   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="   crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <section id="home">
+        <div class="ui center aligned header">
+            <div class="logo">
+                <img src="assets/logo/MikaLogo.png" alt="Logo">
+            </div>
+        </div>
+        <div class="ui center aligned buttons container">
+            <a href="">
+                <div class="ui massive inverted button">
+                    Log in
+                </div>
+            </a>
+            <a href="">
+                <div class="ui massive green button">
+                    Register
+                </div>
+            </a>
+        </div>
+    </section>
+</body>
+</html>
